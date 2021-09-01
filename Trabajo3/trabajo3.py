920d17b5e1c6be82319ee3075e0ef60157ac9bd0
Evert Acosta PC <evertjosea@gmail.com>
2021-09-01 00:12:00 -0500

    Inicia respuestas finales para el trabajo antes de eliminar las anteriores

--- Trabajo3/trabajo3.py
+++ Trabajo3/trabajo3.py
@@ -1,8 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def graficar(t, y_analitica, y_om, title):
+    fig, ax = plt.subplots()
+    ax.plot(t, y_analitica)
+    ax.plot(t, y_om)
+    ax.title(title)
+    ax.legend()
+
+
+
+
 
 
 def raizes(T1, T2):
-
+    """
+    Calcula las raizes solucion correspondiente a la ecuacion diferencial
+    :param T1: Tiempo de estabilizacion T1 entregado por el usuario
+    :param T2: Tiempo de estabilizacion T2 entregado por el usuario
+    :return: Raizes solucion de la ecuacion diferencial
+    """
     T1 = T1
     T2 = T2
 
@@ -12,7 +31,12 @@ def raizes(T1, T2):
     return raiz1, raiz2
 
 
-def coeficientes(m, r):
+def coeficientes(r, m):
+    """
+    :param r:
+    :param m:
+    :return:
+    """
     b = r * 1/m
     k = 1/m
     s = -(r**2)
@@ -34,10 +58,12 @@ def sobreamortiguado(T1, T2, m):
 
     r1, r2 = raizes(T1, T2)
 
-    b1, k1, s1 = coeficientes(m, r1)
+    b1, k1, s1 = coeficientes(r1, m)
 
-    b2, k2, s2 = coeficientes(m, r2)
+    b2, k2, s2 = coeficientes(r2, m)
     solution = np.around(solucion_sistema(b1, k1, s1, b2, k2, s2), 2)
+
+    # retorna b, k
     return solution[0], solution[1]
 
 
@@ -46,10 +72,35 @@ def sigma(Ts):
 
 
 def omega(Ps):
+    """
+    :param Ps:
+    :return:
+    """
     return (2*np.pi)/Ps
 
 
+# Constante de amortiguaminto
 def b(sigma, m):
     return -(sigma*2*m)
 
-# def k(b,m):
+
+# constante del resorte
+def k(omega, b, m):
+    return m*(omega**2)+((b**2)/(4*m))
+
+
+def subamortiguado(Ts, Ps, m):
+
+    s = sigma(Ts)
+
+    o = omega(Ps)
+
+    cb = b(s, m)
+
+    ck = k(o, cb, m)
+
+    # retorna b, k
+    return np.around(cb, 2), np.around(ck, 2)
+
+
+
--- test/test_trabajo3.py
+++ test/test_trabajo3.py
@@ -1,6 +1,17 @@
 import trabajo3
+import numpy as np
+
+def test_sobreamortiguado1():
+    assert trabajo3.sobreamortiguado(9.0, 7.0, 10.0) == (12.70, 3.97)
 
 
-def test_sobreamortiguado():
+def test_sobreamortiguado23():
+    assert trabajo3.sobreamortiguado(5/2, 5/8, 1.0) == (10.0, 16.0)
 
-    assert trabajo3.sobreamortiguado(9, 7, 10) == (12.70, 3.97)
+
+def test_subamortiguado1():
+    assert trabajo3.subamortiguado(4.0, 1/2, 10.0) == (25.0, 1594.76)
+
+
+def test_subamortiguado25():
+    assert trabajo3.subamortiguado(2.5, np.pi/2, 0.1) == (0.4, 2.0)
